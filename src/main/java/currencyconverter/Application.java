7508eefd1bcfb2cfe50379af1d4886f10588455f
amclau303 <amclau@caledonian.ac.uk>
2022-11-15 16:46:03 +0000

    remove application class

--- src/main/java/currencyconverter/Application.java
+++
@@ -1,7 +0,0 @@
-package currencyconverter;
-
-public class Application {
-    public static void main(String[] args) throws Exception {
-        CurrencyConverter Application = new CurrencyConverter(args);
-    }
-}
